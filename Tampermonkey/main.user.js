be31cb8b0c1bfc49e41b9846379fedf1cf30c75e
HKG <hkg902@gmail.com>
2019-06-13 23:55:33 +0200

    Add meta to allow updating.

--- Tampermonkey/main.user.js
+++ Tampermonkey/main.user.js
@@ -5,7 +5,8 @@
 // @description  should be self-explanatory.
 // @author       HKG
 // @match        https://www.crunchyroll.com/*/*
-// @grant        none
+// @downloadURL  https://github.com/HKGx/Crunchyroll-Skip-Intro/raw/master/Tampermonkey/main.user.js
+// @updateURL    https://github.com/HKGx/Crunchyroll-Skip-Intro/raw/master/Tampermonkey/meta.user.js
 // ==/UserScript==
 
 var player;
